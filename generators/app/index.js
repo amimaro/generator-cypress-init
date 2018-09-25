884136fdb7f70918153b1a7469be5bbcbb774c99
Amimaro <amir.zahlan@gmail.com>
2018-09-25 10:37:18 -0300

    :sparkles: Add tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,15 +6,15 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the best ${chalk.red('generator-cypress-init')} generator!`)
-    );
+    this.log(yosay(`Welcome to ${chalk.green('generator-cypress-init')}!`));
 
     const prompts = [
       {
         type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
+        name: 'files',
+        message: `Create tests folder at root directory and config script at ${chalk.red(
+          'package.json'
+        )}?`,
         default: true
       }
     ];
@@ -26,13 +26,28 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    this.fs.copy(this.templatePath('cypress.json'), this.destinationPath('cypress.json'));
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('tests/e2e/plugins'),
+      this.destinationPath('tests/e2e/plugins')
+    );
+    this.fs.copy(
+      this.templatePath('tests/e2e/specs'),
+      this.destinationPath('tests/e2e/specs')
+    );
+    this.fs.copy(
+      this.templatePath('tests/e2e/support'),
+      this.destinationPath('tests/e2e/support')
+    );
+    this.fs.copy(
+      this.templatePath('tests/e2e/_.eslintrc.js'),
+      this.destinationPath('.eslintrc.js')
     );
   }
 
   install() {
-    this.installDependencies();
+    this.npmInstall(['cypress'], {
+      'save-dev': true
+    });
   }
 };
