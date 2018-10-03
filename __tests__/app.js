ae2f7a6892e061f1391082770d4781248c9f4b97
amimaro <amir.zahlan@gmail.com>
2018-10-03 07:08:53 -0300

    Test baseUrl

--- __tests__/app.js
+++ __tests__/app.js
@@ -7,10 +7,14 @@ describe('generator-cypress-init:app', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ files: true });
+      .withPrompts({ files: true, baseUrl: 'baseUrl' });
   });
 
   it('creates files', () => {
     assert.file(['./tests/e2e/specs/test.js']);
+  });
+
+  it('set baseUrl', () => {
+    assert.fileContent('./cypress.json', 'baseUrl');
   });
 });
